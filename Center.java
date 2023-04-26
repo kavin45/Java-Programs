941e34950b62ad4c6c4fc6596afeb28763d0e5f9
kavin45 <52598647+kavin45@users.noreply.github.com>
2023-04-26 05:24:44 -0400

    Add files via upload

---
+++ Center.java
@@ -0,0 +1,11 @@
+class Center
+{
+    public static void main(String args[])
+    {
+     int a=734 ,n,N;
+     n=a/10;
+     N=n%10;
+     System.out.println("The a valiue is :"+a); 
+     System.out.println("The Value :"+N);
+    }
+}
